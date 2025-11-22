c47d04673ba53f1ad9bfbc72c9fb71fde0609a6a
Kevin-Michael-pacheco <kevinmanasses@gmail.com>
2025-11-21 23:34:19 -0300

    Implement user listing and removal functions
    Added functions to list and remove users from the registration list.

--- Sistema de cadastro simples.py
+++ Sistema de cadastro simples.py
@@ -1,10 +1,68 @@
 lista = []
-
 def cadastrar():
     while True: 
         n=input('digite o  nome: ')
         lista.append(n)
-        condicao = input("quer continuar digitando nomes  s/n: ")
-        if condicao == "n" or condicao == "N":
+        condicao = input("quer continuar digitando nomes  s/n: ").strip().lower()
+        if condicao == "n" :
+            print()
             break
-cadastrar()
+    
+def listar():
+    print()
+    if lista:
+        print('==========nomes cadastrados============')
+        for i in lista:
+            print(i)
+    else:
+        print('nenhum nome encontrado')
+        print()
+        print('cadastre os nomes antes de tentar listar eles ')
+    print()
+
+
+
+def remover_usuario():
+    print()
+    nome=str(input('digite o nome que deseja remover: '))
+    if not lista:
+        print('a lista esta vazia')
+    else:
+        if nome in lista:
+            lista.remove(nome)
+            print('nome removido com sucesso')
+        else:
+            print('o item não esta na lista')
+
+        
+def main():
+    while True:
+        print('================Menu===================')
+        print('1- cadastrar nomes')
+        print('2- listar nomes cadastrados')
+        print('3-remover nomes')
+        print('4-sair')
+        print('========================================')
+        try:
+            op=int(input('digite a opção que deseja : '))
+        except:
+            print('opção invalida')
+            continue
+        if op == 1:
+            cadastrar()
+        elif op == 2:
+            listar()
+        elif op == 3:
+            remover_usuario()
+        elif op == 4:
+            print('programa encerrado')
+            break
+        else:
+            print('opção invalida digite apenas opções validas')
+main()
+
+
+
+
+    
+
